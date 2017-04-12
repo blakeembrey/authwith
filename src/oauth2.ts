3cd8a24f9859c7b638e6c27c81e05a476aed800d
Blake Embrey <hello@blakeembrey.com>
2017-04-11 18:01:43 -0700

    .

---
+++ src/oauth2.ts
@@ -0,0 +1,154 @@
+import { map } from 'map-pointer'
+import { parse as parseUrl } from 'url'
+import { stringify as stringifyQuery, parse as parseQuery } from 'querystring'
+import { AuthError, ProfileMap, Profile, request } from './support'
+
+/**
+ * Required configuration for OAuth 2.0 providers.
+ */
+export interface OAuth2Provider {
+  accessTokenUri: string
+  authorizationUri: string
+  profileUri: string
+  profileMap: ProfileMap
+}
+
+/**
+ * Shape of the authorization step.
+ */
+export interface OAuth2Authorization {
+  access_token: string
+  token_type?: string
+  refresh_token?: string
+  expires_in?: string
+}
+
+/**
+ * Authentication parameters passed between methods.
+ */
+export interface OAuth2Params {
+  clientId: string
+  clientSecret: string
+  redirectUri: string
+  scope: string
+  state?: string
+}
+
+/**
+ * Default OAuth 2.0 headers.
+ */
+export const DEFAULT_HEADERS = {
+  'Accept': 'application/json, application/x-www-form-urlencoded',
+  'Content-Type': 'application/x-www-form-urlencoded'
+}
+
+/**
+ * Attempt to parse the payload body as JSON or URL-encoding.
+ */
+export function parseResponseBody (body: string) {
+  try {
+    return JSON.parse(body)
+  } catch (e) {
+    return parseQuery(body)
+  }
+}
+
+/**
+ * Check the body for an authentication error.
+ */
+export function getAuthError (body: any): AuthError | void {
+  if (body.error || body.error_description) {
+    return new AuthError('oauth2', body.error_description || body.error, body.error, body.error_uri)
+  }
+}
+
+export class OAuth2 {
+
+  constructor (public provider: OAuth2Provider) {}
+
+  getRedirectUri (params: OAuth2Params) {
+    const query = {
+      client_id: params.clientId,
+      redirect_uri: params.redirectUri,
+      scope: params.scope,
+      response_type: 'code',
+      state: params.state
+    }
+
+    return `${this.provider.authorizationUri}?${stringifyQuery(query)}`
+  }
+
+  getToken (callbackUri: string, params: OAuth2Params): Promise<OAuth2Authorization> {
+    const { query } = parseUrl(callbackUri, true)
+    const err = getAuthError(query)
+
+    if (err) {
+      return Promise.reject(err)
+    }
+
+    if (params.state && query.state !== params.state) {
+      return Promise.reject(new AuthError('oauth2', 'OAuth 2.0 state mismatch'))
+    }
+
+    if (!query.code) {
+      return Promise.reject(new AuthError('oauth2', 'Missing OAuth 2.0 code'))
+    }
+
+    return request({
+      url: this.provider.accessTokenUri,
+      method: 'POST',
+      headers: DEFAULT_HEADERS,
+      body: stringifyQuery({
+        code: query.code,
+        grant_type: 'authorization_code',
+        redirect_uri: params.redirectUri,
+        client_id: params.clientId,
+        client_secret: params.clientSecret
+      })
+    })
+      .then((res) => {
+        const body = parseResponseBody(res.body)
+        const err = getAuthError(body)
+
+        if (err) {
+          return Promise.reject(err)
+        }
+
+        if (res.status !== 200) {
+          return Promise.reject(new AuthError('oauth2', `Invalid response status: ${res.status}`))
+        }
+
+        if (!body.access_token) {
+          return Promise.reject(new AuthError('oauth2', 'Unable to retrieve access token from response'))
+        }
+
+        return body
+      })
+  }
+
+  getProfile (token: OAuth2Authorization, _params: OAuth2Params) {
+    return request({
+      url: this.provider.profileUri,
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${token.access_token}`,
+        ...DEFAULT_HEADERS
+      }
+    })
+      .then((res) => {
+        if (res.status !== 200) {
+          return Promise.reject<Profile>(new AuthError('oauth2', `Invalid response status: ${res.status}`))
+        }
+
+        const body = parseResponseBody(res.body)
+        const profile = map<Profile>(this.provider.profileMap, body)
+
+        if (!profile.sub) {
+          return Promise.reject<Profile>(new AuthError('oauth2', 'No profile sub'))
+        }
+
+        return profile
+      })
+  }
+
+}
---
+++ src/oidc.ts
@@ -0,0 +1,162 @@
+import jsonwebtoken = require('jsonwebtoken')
+import { map } from 'map-pointer'
+import { stringify as stringifyQuery } from 'querystring'
+import { AuthError, ProfileMap, Profile } from './support'
+import { OAuth2, OAuth2Provider, OAuth2Authorization, OAuth2Params } from './oauth2'
+
+/**
+ * Provider configuration.
+ */
+export interface OpenIDConnectProvider {
+  accessTokenUri: string
+  authorizationUri: string
+  issuer: string
+  profileMap?: ProfileMap
+}
+
+/**
+ * Authorization response.
+ */
+export interface OpenIDConnectAuthorization extends OAuth2Authorization {
+  id_token: string
+}
+
+/**
+ * Parameters are similar to "local state" and should be passed to each method.
+ */
+export interface OpenIDConnectParams extends OAuth2Params {
+  nonce?: string
+  // Specifies the allowable elapsed time in ms.
+  maxAge?: number
+  // Current timestamp used alongside `maxAge` for verification.
+  timestamp?: number
+}
+
+/**
+ * Default profile map for OpenID Connect.
+ */
+const OIDC_PROFILE_MAP: ProfileMap = {
+  sub: '/sub',
+  name: '/name',
+  givenName: '/given_name',
+  familyName: '/family_name',
+  middleName: '/middle_name',
+  nickname: '/nickname',
+  preferredUsername: '/preferred_username',
+  profile: '/profile',
+  picture: '/picture',
+  website: '/website',
+  email: '/email',
+  emailVerified: '/email_verified',
+  gender: '/gender',
+  birthdate: '/birthdate',
+  zoneinfo: '/zoneinfo',
+  locale: '/locale',
+  phoneNumber: '/phone_number',
+  phoneNumberVerified: '/phone_number_verified',
+  updatedAt: '/updated_at'
+}
+
+export class OpenIDConnect extends OAuth2 {
+
+  provider: OpenIDConnectProvider & OAuth2Provider
+
+  constructor (_provider: OpenIDConnectProvider) {
+    const provider: OAuth2Provider = Object.assign({
+      profileMap: OIDC_PROFILE_MAP,
+      profileUri: ''
+    }, _provider)
+
+    super(provider)
+  }
+
+  getRedirectUri (params: OpenIDConnectParams) {
+    if (!/\bopenid\b/.test(params.scope)) {
+      throw new TypeError(`Scope "${params.scope}" must contain "openid"`)
+    }
+
+    const query = {
+      client_id: params.clientId,
+      redirect_uri: params.redirectUri,
+      scope: params.scope || 'openid',
+      response_type: 'code',
+      state: params.state,
+      nonce: params.nonce,
+      max_age: typeof params.maxAge === 'number' ? ~~(params.maxAge / 1000) : undefined
+    }
+
+    return `${this.provider.authorizationUri}?${stringifyQuery(query)}`
+  }
+
+  getToken (callbackUri: string, params: OpenIDConnectParams): Promise<OpenIDConnectAuthorization> {
+    return super.getToken(callbackUri, params)
+  }
+
+  // Based on https://github.com/jaredhanson/passport-openidconnect/blob/ac1c0257f02353f818be33c0602cca5883d97235/lib/strategy.js.
+  getProfile (token: OpenIDConnectAuthorization, params: OpenIDConnectParams) {
+    const jwtClaims = jsonwebtoken.decode(token.id_token)
+    const missing = ['iss', 'sub', 'aud', 'exp', 'iat'].filter((param) => !jwtClaims[param])
+
+    if (missing.length) {
+      return Promise.reject<Profile>(new AuthError('oidc', `ID token missing required parameters: ${missing.join(', ')}`))
+    }
+
+    // https://openid.net/specs/openid-connect-basic-1_0.html#IDTokenValidation - check 1.
+    if (jwtClaims.iss !== this.provider.issuer) {
+      return Promise.reject<Profile>(new AuthError('oidc', `ID token not issued by correct OpenID provider: ${jwtClaims.iss}`))
+    }
+
+    // https://openid.net/specs/openid-connect-basic-1_0.html#IDTokenValidation - checks 2 and 3.
+    if (typeof jwtClaims.aud === 'string') {
+      if (jwtClaims.aud !== params.clientId) {
+        return Promise.reject<Profile>(new AuthError('oidc', `Audience parameter is for a different client: ${jwtClaims.aud}`))
+      }
+    } else if (Array.isArray(jwtClaims.aud)) {
+      if (jwtClaims.aud.indexOf(params.clientId) === -1) {
+        return Promise.reject<Profile>(new AuthError('oidc', `Audience parameter does not include this client: ${jwtClaims.aud.join(', ')}`))
+      }
+
+      if (jwtClaims.aud.length > 1 && !jwtClaims.azp) {
+        return Promise.reject<Profile>(new AuthError('oidc', '`azp` parameter required with multiple audiences'))
+      }
+    } else {
+      return Promise.reject<Profile>(new AuthError('oidc', 'Invalid `aud` parameter'))
+    }
+
+    // https://openid.net/specs/openid-connect-basic-1_0.html#IDTokenValidation - check 4.
+    if (jwtClaims.azp && jwtClaims.azp !== params.clientId) {
+      return Promise.reject<Profile>(new AuthError('oidc', `This client is not the authorized party: ${jwtClaims.azp}`))
+    }
+
+    // https://openid.net/specs/openid-connect-basic-1_0.html#IDTokenValidation - check 5.
+    if (jwtClaims.exp < (Date.now() / 1000)) {
+      return Promise.reject<Profile>(new AuthError('oidc', 'ID token has expired'))
+    }
+
+    // Note: https://openid.net/specs/openid-connect-basic-1_0.html#IDTokenValidation - checks 6 and 7 are out of scope of this library.
+
+    // https://openid.net/specs/openid-connect-basic-1_0.html#IDTokenValidation - check 8.
+    if (params.maxAge) {
+      if (!params.timestamp) {
+        return Promise.reject<Profile>(new TypeError('The timestamp must be provided with `maxAge` parameter'))
+      }
+
+      if (!jwtClaims.auth_time || ((params.timestamp - params.maxAge) / 1000) > jwtClaims.auth_time) {
+        return Promise.reject<Profile>(new AuthError('oidc', 'Auth time is not included or too old'))
+      }
+    }
+
+    if (params.nonce && (!jwtClaims.nonce || jwtClaims.nonce !== params.nonce)) {
+      return Promise.reject<Profile>(new AuthError('oidc', 'Invalid nonce in ID token'))
+    }
+
+    const profile = map<Profile>(this.provider.profileMap, jwtClaims)
+
+    if (!profile.sub) {
+      return Promise.reject<Profile>(new AuthError('oauth2', 'No profile sub'))
+    }
+
+    return Promise.resolve(profile)
+  }
+
+}
